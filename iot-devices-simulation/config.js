a9ef24062f08557f5acb163d2e3bf9462761cd01
krishachikka <chikkakrisha@gmail.com>
2025-09-26 09:20:05 +0530

    done w simulation

--- iot-devices-simulation/config.js
+++ iot-devices-simulation/config.js
@@ -1,6 +1,6 @@
 // Centralized config so you can tweak easily
 module.exports = {
-  NUM_DEVICES: 100,             // number of simulated devices
+  NUM_DEVICES: 10,             // number of simulated devices
   MIN_INTERVAL: 500,           // min send interval in ms
   MAX_INTERVAL: 2000,          // max send interval in ms
   FACTORY_ID: "factory_1"
