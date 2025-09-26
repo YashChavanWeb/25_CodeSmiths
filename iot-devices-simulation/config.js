11d4d5fa64f666f32e530e5a4aaae5bd74e0cfed
krishachikka <chikkakrisha@gmail.com>
2025-09-26 09:14:29 +0530

    done w simulation

---
+++ iot-devices-simulation/config.js
@@ -0,0 +1,7 @@
+// Centralized config so you can tweak easily
+module.exports = {
+  NUM_DEVICES: 100,             // number of simulated devices
+  MIN_INTERVAL: 500,           // min send interval in ms
+  MAX_INTERVAL: 2000,          // max send interval in ms
+  FACTORY_ID: "factory_1"
+};
---
+++ iot-devices-simulation/index.js
@@ -0,0 +1,8 @@
+const { NUM_DEVICES } = require("./config");
+const { createBot } = require("./bots/deviceBot");
+
+console.log(`🚀 Starting IoT Simulator with ${NUM_DEVICES} devices...\n`);
+
+for (let i = 1; i <= NUM_DEVICES; i++) {
+  createBot(i);
+}
