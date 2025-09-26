9003b5bf72d067ab5323235c723f3a9bc2541750
YashChavanWeb <yashchavan4628@gmail.com>
2025-09-26 19:30:26 +0530

    changed the port

--- iot-devices-simulation/backend/config.js
+++ iot-devices-simulation/backend/config.js
@@ -4,7 +4,7 @@ export const MAX_INTERVAL = 2000;
 export const FACTORY_ID = "factory_1";
 export const SERVER_PORT = 5000;
 export const KAFKA_GATEWAY_PORT = 9092;
-export const KAFKA_BROKER = "192.168.137.35:9092";
+export const KAFKA_BROKER = "192.168.160.1:9092";
 export const KAFKA_TOPIC = "sensor-data";
 
 export const SYSTEM_TYPES = [
